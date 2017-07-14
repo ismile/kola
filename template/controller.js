ae81b6240ce7bbb057ca5bf3485e6c132ceeaeb9
Ismail Sunny <ismiletea@gmail.com>
2017-07-14 14:40:02 +0700

    simplify controller generator

--- template/controller.js
+++ template/controller.js
@@ -1,9 +1,17 @@
 import Router from 'koa-router';
 import Controller from '../util/controller';
-import <%= className %> from '../model/<%= className %>';
 
 export default class <%= className %>Controller extends Controller {
 	path = '<%= name %>';
-	Model = <%= className %>;
 
+	/**
+	 * @name hai
+	 * @param {Router.IRouterContext} ctx
+	 * @param {Promise<>} next
+	 * @description POST /api/<%= name %>/hai
+	 */
+	@route.get('/hai')
+	async find(ctx, next) {
+		ctx.body = 'hai'
+	}
 }
